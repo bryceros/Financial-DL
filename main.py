87ceb3293aca1ecd7be601210365b8308f5d1905
slenoski <email@example.com>
2019-11-29 17:17:00 -0800

    updated main file for easier testing

--- main.py
+++ main.py
@@ -22,43 +22,77 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 
+def run_model(is_train=True,model_name='rl_model'):
+    df = pd.read_csv('./data/db.csv')
+    df = df.sort_values('date')
+    df = df.drop(columns='date')
 
-#df = pd.read_csv('./data/AAPL.csv')
-#df = df.sort_values('Date')
+    df = df.dropna().reset_index()
+    print(df.isnull().sum().sum())
 
-#df = parse('./data/pre_data_10years')
+    # train, test = train_test_split(df, test_size=0.1)
 
-df = pd.read_csv('./data/db.csv')
-df = df.sort_values('date')
-df = df.drop(columns='date')
+    train = df[:int(0.9 * len(df))].reset_index()
+    test = df[int(0.9 * len(df)):].reset_index()
+    # The algorithms require a vectorized environment to run
+    env = DummyVecEnv([lambda: StockTradingEnv(train, train, 30, True)])
+    env_test = DummyVecEnv([lambda: StockTradingEnv(test, train, 30, False)])
 
-df = df.dropna().reset_index() 
-print(df.isnull().sum().sum())
+    if is_train and model_name == 'rl_rand_model':
+        model = PPO2(RandomPolicy, env, verbose=11, tensorboard_log="./log/rand_stock_tensorboard/")
 
-#train, test = train_test_split(df, test_size=0.1)
+    elif not is_train and model_name == 'rl_rand_model':
+        model = PPO2.load("./ckpt/rl_rand_model")
 
-train = df[:int(0.9*len(df))].reset_index() 
-test = df[int(0.9*len(df)):].reset_index() 
-# The algorithms require a vectorized environment to run
-env = DummyVecEnv([lambda: StockTradingEnv(train,train,30,True)])
-env_test = DummyVecEnv([lambda: StockTradingEnv(test,train,30,False)])
+    elif is_train and model_name == 'rl_model':
+        model = PPO2(CustomPolicy, env, verbose=11, tensorboard_log="./log/ppo2_stock_tensorboard/")
 
-#model = PPO2(CustomPolicy, env, verbose=11, tensorboard_log="./log/ppo2_stock_tensorboard/")
-#model = PPO2(RandomPolicy, env, verbose=11, tensorboard_log="./log/rand_stock_tensorboard/")
-#model = Baseline(env)
-#model = Heristic(env)
-for epoch in range(1):
+    elif not is_train and model_name == 'rl_model':
+        model = PPO2.load("./ckpt/rl_model")
 
-    obs = env_test.reset()
-    #model.load("./ckpt/rl_model")
-    #model.learn(total_timesteps=500000)
-    #model.save("./ckpt/rl_model")
-    rewards = []
-    for i in range(len(test.loc[:, 'TROW_PRC'].values)-30):
-        print(i)
-        action, _states = model.predict(obs)
-        obs, reward, done, info = env_test.step(action)
-        rewards.append(reward[0])
-        env_test.render()
-    #plt.plot(data=rewards)
-    #plt.show()
+    elif not is_train and model_name == 'hr_model':
+        model = Heristic(env_test)
+    elif is_train and model_name == 'hr_model':
+        model = Heristic(env)
+
+    elif not is_train and model_name == 'rnn_model':
+        model = Baseline(env_test)
+    elif is_train and model_name == 'rnn_model':
+        model = Baseline(env)
+    else:
+        assert False
+
+    if not is_train:
+
+        for epoch in range(1):
+            obs = env_test.reset()
+            rewards = []
+            for i in range(len(test.loc[:, 'TROW_PRC'].values) - 30):
+                action, _states = model.predict(obs)
+                obs, reward, done, info = env_test.step(action)
+                rewards.append(reward[0])
+                env_test.render()
+            plt.plot(rewards)
+            plt.show()
+    else:
+
+        for epoch in range(1):
+            obs = env.reset()
+            if model_name =='rl_model':
+                model.learn(total_timesteps=500000)
+            model.save("./ckpt/"+model_name)
+            rewards = []
+            for i in range(len(test.loc[:, 'TROW_PRC'].values) - 30):
+                action, _states = model.predict(obs)
+                obs, reward, done, info = env.step(action)
+                rewards.append(reward[0])
+                env.render()
+            plt.plot(rewards)
+            plt.show()
+
+
+
+if __name__ == '__main__':
+    run_model()
+    run_model(False)
+
