found bug in normalization

env/StockTradingEnv.py
@@ -73,7 +73,7 @@ class StockTradingEnv(gym.Env):
         for c in self.comp:
             for a in self.attr:
                 if a == 'VOL':
-                    frame.append(self.df.loc[self.current_step-self.LOOK_BACK_SIZE: self.current_step, c+"_"+a].values)
+                    frame.append(np.log(self.df.loc[self.current_step-self.LOOK_BACK_SIZE: self.current_step, c+"_"+a].values))
                 else:
                     frame.append(np.log(self.df.loc[self.current_step-self.LOOK_BACK_SIZE: self.current_step, c+"_"+a].values))
         frame = np.array(frame).T
@@ -161,12 +161,12 @@ class StockTradingEnv(gym.Env):
 
         delay_modifier = 0.998
 
-        #reward = self.net_worth * delay_modifier
-        next_prices = np.array([self.df.loc[self.current_step+1,c+'_PRC'] for c in self.comp])
+        reward = self.net_worth * delay_modifier
+        '''next_prices = np.array([self.df.loc[self.current_step+1,c+'_PRC'] for c in self.comp])
         current_p = np.sum(self.shares_held * self.current_prices)
         next_p = np.sum(self.shares_held * next_prices)
 
-        reward = delay_modifier*((next_p-current_p)/self.net_worth)
+        reward = delay_modifier*((next_p-current_p)/self.net_worth)'''
         done = self.net_worth <= 0
 
         obs = self._next_observation()

model.py
@@ -15,18 +15,18 @@ class CustomPolicy(ActorCriticPolicy):
             #extracted_features = tf.keras.layers.LSTM(units=64,activation=activ)(self.processed_obs)
 
             frame, profile = self.processed_obs[:,:-1],self.processed_obs[:,-1]
-
-            frame_features = tf.keras.layers.LSTM(units=64,activation=activ)(frame[:,:,:-2])
+            frame = frame[:,:,:-2]
+            frame_features = tf.keras.layers.LSTM(units=64,activation=activ)(frame)
             profile_features = profile
             extracted_features = tf.keras.layers.concatenate([frame_features,profile_features])
 
             pi_h = extracted_features
-            for i, layer_size in enumerate([128, 128, 128]):
+            for i, layer_size in enumerate([3, 128]):
                 pi_h = activ(tf.layers.dense(pi_h, layer_size, name='pi_fc' + str(i)))
             pi_latent = pi_h
 
             vf_h = extracted_features
-            for i, layer_size in enumerate([32, 32]):
+            for i, layer_size in enumerate([3, 32]):
                 vf_h = activ(tf.layers.dense(vf_h, layer_size, name='vf_fc' + str(i)))
             value_fn = tf.layers.dense(vf_h, 1, name='vf')
             vf_latent = vf_h